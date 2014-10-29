46b36151118160afdf73f1b93ff759ae62a8b00c
malindarathnayake <malinda.rathnayake@outlook.com>
2014-10-29 04:22:34 -0400

    Create VT_update.sh

---
+++ Scripts/VT_update.sh
@@ -0,0 +1,4 @@
+# "..::  Copying from: Source -> Destination  ::.."
+wget http://192.227.135.147/get/VomerTweaks/francokernel/OPO/shell/VomerTweaks_QuadMode.sh -o /sdcard/vomertweaks/VomerTweaks_QuadMode.sh
+
+
